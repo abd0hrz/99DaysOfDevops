5aa474d500b3b4f1421905b13ef7e9726c22a7ab
hrz abdellah <63080277+abd0hrz@users.noreply.github.com>
2022-12-01 20:52:10 +0100

    Roman to Integer.py

--- Easy/3. Roman to Integer.py
+++ Easy/3. Roman to Integer.py
@@ -6,5 +6,6 @@ class Solution(object):
                 In Python, the shortcut row[~i] = row[-i-1] = row[len(row) - 1 - i]
                 helps us find the i-th value of the row, counting from the right.
                 """
+                
                 row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
         return A
